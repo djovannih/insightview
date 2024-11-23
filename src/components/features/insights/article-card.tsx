04f96998ef9badee7dab50dfff666f20723ae4b1
Giovanni Improta <bigbossgion@gmail.com>
2024-11-23 19:46:57 +0100

    feat(article-card): copy article to clipboard

--- src/components/features/insights/article-card.tsx
+++ src/components/features/insights/article-card.tsx
@@ -1,6 +1,17 @@
+import { Copy } from "lucide-react";
+import { useTranslations } from "next-intl";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import ErrorMessage from "@/components/features/insights/error-message";
 
 interface ArticleCardProps {
@@ -16,16 +27,46 @@ export default function ArticleCard({
   error,
   retry,
 }: ArticleCardProps) {
+  const t = useTranslations("ArticleCard");
+
+  const copyToClipboard = async () => {
+    if (articleHtml) {
+      const promise = navigator.clipboard.writeText(articleHtml);
+      toast.promise(promise, {
+        success: t("articleCopied"),
+        error: t("failedToCopyArticle"),
+      });
+      await promise;
+    }
+  };
+
   return (
     <Card>
       <CardContent className="flex flex-col">
         {articleHtml && !loading && !error && (
-          <ScrollArea>
+          <ScrollArea className="group">
             <div
               className="h-96 pr-4"
               dangerouslySetInnerHTML={{ __html: articleHtml }}
               style={{}}
             />
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={copyToClipboard}
+                    className="absolute bottom-0 right-4 opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+                  >
+                    <Copy />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{t("copyToClipboard")}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
             <style jsx>{`
               div :global(p) {
                 margin-bottom: 1.2rem;
